3ac3f565d30901d28f5fed961f1ea199fd0dfd0c
caio <nreis.caio@gmail.com>
2022-02-22 15:37:11 -0300

    s

---
+++ Atividade012/projeto012_a/operacao.py
@@ -0,0 +1,11 @@
+class Operacao:
+
+    def __init__(self, a, b):
+
+        self.a = a
+        self.b = b
+    
+    def divisao(a, b):
+
+        divisao = a / b
+        return divisao
---
+++ Atividade012/projeto012_c/conversor.py
@@ -0,0 +1,20 @@
+class Conversor:
+
+    def __init__(self, a):
+
+        self.a = a
+
+    def conversorDolar(a):
+
+        valorDolar = a / 5.10
+        return valorDolar
+
+    def conversorEuro(a):
+
+        valorEuro = a / 5.77
+        return valorEuro
+
+    def conversorLibra(a):
+        
+        valorLibra = a / 6.94
+        return valorLibra
