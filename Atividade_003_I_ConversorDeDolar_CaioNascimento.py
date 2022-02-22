c17f708af137e6524172074f1ddb66a3b738ffc1
caio <nreis.caio@gmail.com>
2022-02-22 00:15:35 -0300

    Aula Senac

---
+++ Atividade_003_I_ConversorDeDolar_CaioNascimento.py
@@ -0,0 +1,25 @@
+#Curso Tecnico de Informatica
+#Autor: Caio Nascimento
+#Data inicio: 28/10/2021
+#Data Termino: 28/10/2021
+#Atividade 003: Exercicio I = Tabuada de mutiplicação.
+
+#Entrada:
+print('-'*50)
+print('CONVERSOR DE REAIS EM DOLAR')
+print('='*50)
+
+#Entrada de dados:
+a = float(input('Por favor, insira um valor em reais: '))
+
+#Operações:
+conversao = a / 5.6123998 
+
+#Saída:
+print('-'*50)
+print(f'Com {a} BRL da para comprar: {conversao:.4f} USD')
+
+#Texto de encerramento:
+print('-'*40)
+print('FIM DO PROGRAMA')
+print('='*50)
